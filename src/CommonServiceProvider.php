74c02ee6e8444fe22d257f399a7a20cb74e9a683
Omer Šabić <omer@datalinx.si>
2025-08-07 15:32:10 +0200

    fix: fix nav menu labels

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -22,6 +22,9 @@ class CommonServiceProvider extends PackageServiceProvider
         // Plugin class must be set as singleton
         $this->app->singleton(CommonPlugin::class);
 
+        // Set translation loading in register method, so that the nav file is ready when the panel is being configured
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'eclipse-common');;
+
         return $this;
     }
 }

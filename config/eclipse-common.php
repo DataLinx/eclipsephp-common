9150ff9181f5c5be0e3d8a922914447751e0444b
SlimDeluxe <131700+SlimDeluxe@users.noreply.github.com>
2026-09-04 14:07:48 +0000

    style: fix code style

--- config/eclipse-common.php
+++ config/eclipse-common.php
@@ -1,10 +1,13 @@
 <?php
 
+use Eclipse\Common\Support\Callback;
+
 return [
     /**
      * Available locales for the application.
      * Can be a list (array) of locale codes or a Callback instance.
-     * @see \Eclipse\Common\Support\Callback
+     *
+     * @see Callback
      */
     'available_locales' => ['en', 'sl'],
 ];
